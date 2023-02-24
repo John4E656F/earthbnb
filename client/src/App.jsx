b7343c5d338a26bb526dcb0a8f1f043481b365c1
John4E656F <jm49andres@gmail.com>
2023-02-24 13:36:39 +0100

    Initialize

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './pages/layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
